2d1bc0901457d383b204a0dbba2a0741afcbbdd1
Lucas Chabiron <84028247+lucaschabiron@users.noreply.github.com>
2022-09-16 00:21:57 +0200

    Added final changes to db

--- src/data.jsx
+++ src/data.jsx
@@ -6,17 +6,27 @@ export default [
     dates: "10 June 2019 - 25 June 2019",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nisl nunc aliquam nisl, eget aliquam nisl nisl sit amet nisl. Donec auctor, nisl eget ultricies lacinia, nisl nunc aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
-    map: "https://www.google.fr/maps/place/Paris/@48.8589959,2.2069529,11z/data=!3m1!4b1!4m5!3m4!1s0x47e66e1f06e2b70f:0x40b82c3688c9460!8m2!3d48.856614!4d2.3522219",
+    map: "https://www.google.fr/maps/place/Paris/",
     image: "../../Paris.jpg",
   },
   {
-    id: 1,
+    id: 2,
     country: "ITALY",
     title: "Roma",
     dates: "20 May 2020 - 25 May 2020",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nisl nunc aliquam nisl, eget aliquam nisl nisl sit amet nisl. Donec auctor, nisl eget ultricies lacinia, nisl nunc aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
-    map: "https://www.google.fr/maps/place/Paris/@48.8589959,2.2069529,11z/data=!3m1!4b1!4m5!3m4!1s0x47e66e1f06e2b70f:0x40b82c3688c9460!8m2!3d48.856614!4d2.3522219",
+    map: "https://www.google.fr/maps/place/Rome",
     image: "../../roma.jpg",
+  },
+  {
+    id: 3,
+    country: "JAPAN",
+    title: "Tokyo",
+    dates: "22 June 2023 - 12 June 2023",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nisl nunc aliquam nisl, eget aliquam nisl nisl sit amet nisl. Donec auctor, nisl eget ultricies lacinia, nisl nunc aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
+    map: "https://www.google.fr/maps/place/Tokyo",
+    image: "../../tokyo.jpg",
   },
 ];
